196f6df95990f52c661e5d214fc1a32ca268591a
altarazico <tarazieducation@gmail.com>
2026-03-10 12:38:23 +0300

    Add Person, Student, and Faculty base classes

---
+++ src/main/java/org/example/model/Faculty.java
@@ -0,0 +1,8 @@
+package org.example.model;
+
+public class Faculty extends Person {
+
+    public Faculty (int personId, String name) {
+        super(personId, name);
+    }
+}
--- src/main/java/org/example/model/Student.java
+++ src/main/java/org/example/model/Student.java
@@ -3,6 +3,6 @@ package org.example.model;
 public class Student extends Person {
 
     public Student(int personId, String name) {
-        super(personId,name);
+        super(personId, name);
     }
 }
