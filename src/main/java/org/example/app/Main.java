0a5fdabfad719f2e58f7d7587ab78f65cf8e6262
altarazico <tarazieducation@gmail.com>
2026-03-10 20:15:28 +0300

    Add Person, Student, and Faculty base classes

--- src/main/java/org/example/app/Main.java
+++ src/main/java/org/example/app/Main.java
@@ -1,9 +1,21 @@
 package org.example.app;
 
+import org.example.model.Course;
+import org.example.model.Student;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        Student student1 = new Student(001, "Mike");
+        Student student2 = new Student(002, "Mansour");
+
+        Course course1 = new Course(001, "Introduction to CS");
+
+        course1.addStudent(student1);
+        course1.addStudent(student2);
+
+        course1.printCourseInfo();
     }
 }
